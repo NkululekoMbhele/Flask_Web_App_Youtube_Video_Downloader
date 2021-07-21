0ee8b3810b6f802af358133a1cb707ee2eec8e4f
Nkululeko Mbhele <47152995+NkululekoMbhele@users.noreply.github.com>
2021-07-21 09:18:05 +0200

    update debug to false

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def handle_data():
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False)
